8813c63ea665d4e83e562dbbf3357313c3fad608
John Luong <johnathanluong7@gmail.com>
2023-12-13 02:52:49 -0500

    trying github secrets

--- src/components/Contacts/index.js
+++ src/components/Contacts/index.js
@@ -1,10 +1,12 @@
 import Loader from 'react-loaders';
 import './index.scss';
 import AnimatedLetters from '../AnimatedLetters';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
     const [letterClass, setLetterClass] = useState('text-animate');
+    const refForm = useRef();
 
     useEffect(() => {
         setTimeout(() => {
@@ -12,6 +14,26 @@ const Contact = () => {
         }, 3000)
     }, [])
 
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm(
+            'service_nhuznjf',
+            'template_n6l6165',
+            refForm.current,
+            'v0Q7dts-YGgyaS5L-'
+        )
+        .then(
+            () => {
+                alert('Message was successfully send!');
+                window.location.reload(false);
+            },
+            () => {
+                alert('Failed to send the message, please try again.');
+            }
+        )
+    }
     
     return (
         <> 
@@ -24,7 +46,7 @@ const Contact = () => {
                         If you have any questions or concerns, please don't hesitate to contact me using the form below!
                     </p>
                     <div className='contact-form'>
-                        <form>
+                        <form ref={refForm} onSubmit={sendEmail}>
                             <ul>
                                 <li className='half'>
                                     <input type="text" name='name' placeholder='Name' required />
